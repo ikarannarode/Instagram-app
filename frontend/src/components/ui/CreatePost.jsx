a6400ed5e4d6f922843bde070eb18426fcdd9beb
Karan Narode <karannarode999@gmail.com>
2024-08-29 23:00:20 +0530

    add post controller bug fix

--- frontend/src/components/ui/CreatePost.jsx
+++ frontend/src/components/ui/CreatePost.jsx
@@ -3,84 +3,91 @@ import { Dialog, DialogContent, DialogHeader } from './dialog';
 import { Avatar, AvatarImage, AvatarFallback } from './avatar';
 import { Textarea } from './textarea';
 import { Button } from './button';
-
 import { readFileAsDataURL } from '@/lib/utils';
-import { CloudCog, Loader2 } from 'lucide-react';
+import { Loader2 } from 'lucide-react';
 import { toast } from 'sonner';
 import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { setPosts } from '@/redux/postSlice';
 
-function CreatePost({ open, setOpen }) {
+const CreatePost = ({ open, setOpen }) => {
     const imageRef = useRef();
     const [file, setFile] = useState("");
     const [caption, setCaption] = useState("");
     const [imagePreview, setImagePreview] = useState("");
     const [loading, setLoading] = useState(false);
+    const { user } = useSelector(store => store.auth);
+    const { posts } = useSelector(store => store.post);
+    const dispatch = useDispatch();
 
     const fileChangeHandler = async (e) => {
         const file = e.target.files?.[0];
         if (file) {
             setFile(file);
             const dataUrl = await readFileAsDataURL(file);
-            setImagePreview(dataUrl)
+            setImagePreview(dataUrl);
         }
     }
+
     const createPostHandler = async (e) => {
         const formData = new FormData();
         formData.append("caption", caption);
-        if (imagePreview) formData.append("image", file)
-        e.preventDefault();
+        if (imagePreview) formData.append("image", file);
         try {
             setLoading(true);
-            const response = await axios.post('http://localhost:8000/api/v1/user/profile/edit', formData, {
+            const res = await axios.post('https://instaclone-g9h5.onrender.com/api/v1/post/addpost', formData, {
                 headers: {
-                    'Content-Type': "multipart/form-data"
+                    'Content-Type': 'multipart/form-data'
                 },
                 withCredentials: true
             });
+
             if (res.data.success) {
+                dispatch(setPosts([res.data.post, ...posts]));// [1] -> [1,2] -> total element = 2
                 toast.success(res.data.message);
+                setOpen(false);
             }
+
         } catch (error) {
             toast.error(error.response.data.message);
-        }
-        finally {
+        } finally {
             setLoading(false);
         }
     }
+
     return (
-        <Dialog open={open} className="outline-none border-0">
-            <DialogContent onInteractOutside={() => setOpen(false)} className="outline-none border-0">
-                <DialogHeader className='text-center font-semibold'>
-                    Create New Post
-                </DialogHeader>
+        <Dialog open={open}>
+            <DialogContent onInteractOutside={() => setOpen(false)}>
+                <DialogHeader className='text-center font-semibold'>Create New Post</DialogHeader>
                 <div className='flex gap-3 items-center'>
                     <Avatar>
-                        <AvatarImage src="" alt="img" />
+                        <AvatarImage src={user?.profilePicture} alt="img" />
                         <AvatarFallback>CN</AvatarFallback>
                     </Avatar>
                     <div>
-                        <h1 className='font-semibold text-xs'>Username</h1>
+                        <h1 className='font-semibold text-xs'>{user?.username}</h1>
                         <span className='text-gray-600 text-xs'>Bio here...</span>
                     </div>
                 </div>
-                <Textarea value={caption} onChange={(e) => setCaption(e.target.value)} className='focus-visible:ring-transparent border-none' placeholder='Write a caption...' />
+                <Textarea value={caption} onChange={(e) => setCaption(e.target.value)} className="focus-visible:ring-transparent border-none" placeholder="Write a caption..." />
                 {
-                    imagePreview && (<div className='w-full h-64 flex items-center justify-center'>
-                        <img src={imagePreview} alt="preview_img" className='object-cover h-full w-full rounded-md' />
-                    </div>)
+                    imagePreview && (
+                        <div className='w-full h-64 flex items-center justify-center'>
+                            <img src={imagePreview} alt="preview_img" className='object-cover h-full w-full rounded-md' />
+                        </div>
+                    )
                 }
-                <input type="file" ref={imageRef} name='image' className='hidden' onChange={fileChangeHandler} />
-
-                <Button onClick={() => imageRef.current.click()} className='w-fit mx-auto bg-[#0095F6] hover:bg-[#0081d8] py-2'>Select from Computer</Button>
+                <input ref={imageRef} type='file' className='hidden' onChange={fileChangeHandler} />
+                <Button onClick={() => imageRef.current.click()} className='w-fit mx-auto bg-[#0095F6] hover:bg-[#258bcf] '>Select from computer</Button>
                 {
                     imagePreview && (
                         loading ? (
                             <Button>
                                 <Loader2 className='mr-2 h-4 w-4 animate-spin' />
-                                Loading...
+                                Please wait
                             </Button>
                         ) : (
-                            <Button onClick={createPostHandler} type="submit" className="w-full py-2 bg-cyan-500 hover:bg-cyan-600">Post</Button>
+                            <Button onClick={createPostHandler} type="submit" className="w-full">Post</Button>
                         )
                     )
                 }
